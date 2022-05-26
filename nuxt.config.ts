0a46df94a3f5f7e0aee4e5f672dfa505fad487ae
unnr <wanderingdwarf03@gmail.com>
2022-05-26 17:33:06 +0300

    stylus support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 import { defineNuxtConfig } from 'nuxt3'
-import { devtools } from 'nuxt3/dist/app/compat/capi'
+
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     css: [
-        '~/assets/sass/style.sass'
+        '~/assets/sass/style.sass',
+        '~/assets/stylus/style.styl',
     ],
-})
+})  
